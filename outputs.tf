91bebec44313c79ad317796f7d1771f26e5b1eba
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-12-08 16:34:42 +0000

    vpc-module-development

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,19 @@
 output "vpc_id" {
     value = aws_vpc.main.id
-  
-<<<<<<< HEAD
 }
 
 output "public_subnet_id" {
-    value = aws_subnet.public[*].id
+    value = aws_subnet.public.id
+  
 }
 
+
 output "private_subnet_id" {
-    value = aws_subnet.private[*].id
+    value = aws_subnet.private.id
+  
 }
 
 output "db_subnet_id" {
-    value = aws_subnet.db[*].id
+    value = aws_subnet.db.id
+  
 }
-=======
-}
->>>>>>> 915bd761212ddcf2a0f1080d0e94e41a1e1d8c99
